95e663579e6ccd97310333a1128e46b797855f3e
nawar16 <nawarmk98@gmail.com>
2021-09-02 11:33:37 +0300

    remove paginate

--- app/Http/Controllers/RegulationsAndDecisionsController.php
+++ app/Http/Controllers/RegulationsAndDecisionsController.php
@@ -10,8 +10,13 @@ class RegulationsAndDecisionsController extends Controller
     public function index()
     {
         try {
-            $regulation = Page::where('parent_id', '919')
+            $regulation = Page::where('parent_id', '919')->whereNotIn('id', ['1000'])
             ->orderBy('the_order', 'ASC')->orderBy('id', 'DESC')->get();
+            $circulars = Page::where('id', '1000')->first()->toArray();
+            unset($circulars['pages']);
+            $pages = Page::where('parent_id', '1000')->get();
+            $circulars['pages'] = $pages;
+            $regulation[4] = $circulars;
             return response()->json([
                 'status' => 'success',
                 'data' => $regulation
