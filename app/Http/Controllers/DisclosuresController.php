e0302289dd33fcc8cb3bebdfca021010c6eb6b54
nawar16 <nawarmk98@gmail.com>
2021-09-04 21:53:39 +0300

    company disclosures table

--- app/Http/Controllers/DisclosuresController.php
+++ app/Http/Controllers/DisclosuresController.php
@@ -53,38 +53,40 @@ class DisclosuresController extends Controller
     public function company($id)
     {
         try{
-            $company = Page::findOrFail($id);
-            $disclosure_table = array(); 
-            $company_files = Page::where('parent_id', $company->id)
-            ->where('year', 'like', date("Y"))
-            ->where('Active', 1)
-            ->orderBy('publish_date', 'DESC')
-            ->get();
-            $files = array();
-            foreach($company_files as $p) {
-                $file_type = $p['file_type'];
-                $files[$file_type][] = $p;
-            }
-            if(array_key_exists('first',$files))
-            $res['first'] = $files['first'][0];
-            if(array_key_exists('final',$files))
-            $res['final'] = $files['final'][0];
-            if(array_key_exists('first_quarter',$files))
-            $res['first_quarter'] = $files['first_quarter'][0];
-            if(array_key_exists('half',$files))
-            $res['half'] = $files['half'][0];
-            if(array_key_exists('third_quarter',$files))
-            $res['third_quarter'] = $files['third_quarter'][0];
-            if(array_key_exists('emergency',$files))
-            $res['emergency'] = $files['emergency'][0];
-            if(array_key_exists('public_bodies',$files))
-            $res['public_bodies'] = $files['public_bodies'][0];
-            if(array_key_exists('annual_report',$files))
-            $res['annual_report'] = $files['annual_report'][0];
+            $ans = array();
+            for( $y=2006; $y<=2021; $y++){
+                $company_files = Page::where('parent_id', $id)
+                ->where('year', 'like', $y)
+                ->where('Active', 1)
+                ->orderBy('publish_date', 'DESC')
+                ->get();
+                $files = array();
+                $res = array();
+                foreach($company_files as $p) {
+                    $files[$p['file_type']][] = $p;
+                }
+                if(array_key_exists('first',$files))
+                $res['first'] = $files['first'][0];
+                if(array_key_exists('final',$files))
+                $res['final'] = $files['final'][0];
+                if(array_key_exists('first_quarter',$files))
+                $res['first_quarter'] = $files['first_quarter'][0];
+                if(array_key_exists('half',$files))
+                $res['half'] = $files['half'][0];
+                if(array_key_exists('third_quarter',$files))
+                $res['third_quarter'] = $files['third_quarter'][0];
+                if(array_key_exists('emergency',$files))
+                $res['emergency'] = $files['emergency'][0];
+                if(array_key_exists('public_bodies',$files))
+                $res['public_bodies'] = $files['public_bodies'][0];
+                if(array_key_exists('annual_report',$files))
+                $res['annual_report'] = $files['annual_report'][0];
 
+                $ans[$y] = $res;
+            }
             return response()->json([
                 'status' => 'success',
-                'data' => $res
+                'data' => $ans
             ]);
         } catch(\Exception $ex){
                 return response()->json([
